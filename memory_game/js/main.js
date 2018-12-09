d26447114f9210ab2e236a120462495c8881bd38
Evan <Evan@Macs-MacBook-Pro.local>
2018-12-08 20:06:09 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,24 +1,50 @@
 console.log("Up and running!");
-
-
-var cards = ["queen","queen","king","king"];
+var cardId = []
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank:"queen",
+	suit: "diamonds",
+	cardImage:"images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage:"images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage:"images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 var checkForMatch = function() {
 	if (cardsInPlay.length ===2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-console.log("You found a match!");
+alert("You found a match!");
 } else {
-console.log("Sorry, try again.");
+alert("Sorry, try again.");
 }
 }
 }
 var flipCard = function(cardId) {
-	console.log("user flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch();
+	console.log("user flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 }
-flipCard(0);
-	flipCard(1);
+	
+
+	flipCard(0);
+	flipCard(2);
+	
+
 
 
 
